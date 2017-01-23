8a5fee104b5e75f564854ac6bb9229460dd72f43
Corentin <c.regnier@mentalworks.fr>
2017-01-23 13:47:44 +0100

    #1 Insight correction start

--- deploy/phpmyadmin/config.inc.php
+++ deploy/phpmyadmin/config.inc.php
@@ -76,7 +76,6 @@ if (!empty($dbname)) {
         $cfg['Servers'][$i]['connect_type'] = 'tcp';
         $cfg['Servers'][$i]['port']         = $dbport;
     }
-    //$cfg['Servers'][$i]['compress'] = false;
     /* Select mysqli if your server has it */
     $cfg['Servers'][$i]['extension'] = 'mysqli';
     /* Optional: User for advanced features */
@@ -110,31 +109,6 @@ $cfg['Servers'][$i]['auth_type'] = 'config';
 $cfg['Servers'][$i]['user']      = 'root';
 $cfg['Servers'][$i]['password']  = '1234';
 
-//$cfg['Servers'][$i]['auth_type'] = 'cookie';
-/* Server parameters */
-//$cfg['Servers'][$i]['host'] = 'localhost';
-//$cfg['Servers'][$i]['connect_type'] = 'tcp';
-//$cfg['Servers'][$i]['compress'] = false;
-/* Select mysqli if your server has it */
-//$cfg['Servers'][$i]['extension'] = 'mysql';
-/* Optional: User for advanced features */
-// $cfg['Servers'][$i]['controluser'] = 'pma';
-// $cfg['Servers'][$i]['controlpass'] = 'pmapass';
-
-/* Storage database and tables */
-// $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-// $cfg['Servers'][$i]['bookmarktable'] = 'pma_bookmark';
-// $cfg['Servers'][$i]['relation'] = 'pma_relation';
-// $cfg['Servers'][$i]['table_info'] = 'pma_table_info';
-// $cfg['Servers'][$i]['table_coords'] = 'pma_table_coords';
-// $cfg['Servers'][$i]['pdf_pages'] = 'pma_pdf_pages';
-// $cfg['Servers'][$i]['column_info'] = 'pma_column_info';
-// $cfg['Servers'][$i]['history'] = 'pma_history';
-// $cfg['Servers'][$i]['table_uiprefs'] = 'pma_table_uiprefs';
-// $cfg['Servers'][$i]['tracking'] = 'pma_tracking';
-// $cfg['Servers'][$i]['designer_coords'] = 'pma_designer_coords';
-// $cfg['Servers'][$i]['userconfig'] = 'pma_userconfig';
-// $cfg['Servers'][$i]['recent'] = 'pma_recent';
 /* Uncomment the following to enable logging in to passwordless accounts,
  * after taking note of the associated security risks. */
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
