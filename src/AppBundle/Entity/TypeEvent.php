23206d1d97b5c5bea49380248efc34edae6c22d5
Maxence De Waele <maxencedewaele@MacBook-Pro-de-Maxence.local>
2017-02-10 11:21:09 +0100

    #34 add entity type TypeEvent

---
+++ src/AppBundle/Entity/TypeEvent.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TypeEvent
+ *
+ * @ORM\Table(name="type_event")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TypeEventRepository")
+ */
+class TypeEvent
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return TypeEvent
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
