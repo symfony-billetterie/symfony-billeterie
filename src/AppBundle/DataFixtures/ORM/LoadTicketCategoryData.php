d2d156031b67a5d48ff6f49ca441b9e79d51c825
Pierre-Louis Chevallier <pierrelouis.chevallier60@gmail.com>
2017-02-27 21:49:01 +0100

    Add Entity & Fixtures for CategoryTicket

---
+++ src/AppBundle/DataFixtures/ORM/LoadTicketCategoryData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\TicketCategory;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadTicketCategoryData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $data = array(
+            'Gradin',
+            'Balcon',
+            'Tribune',
+            'Fosse',
+        );
+        
+        foreach ($data as $item => $value) {
+            $ticketCategory = new TicketCategory();
+            $ticketCategory->setLabel($value);
+            $manager->persist($ticketCategory);
+            $this->setReference('TicketCategory-' . $ticketCategory, $value);
+        }
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 0;
+    }
+}
